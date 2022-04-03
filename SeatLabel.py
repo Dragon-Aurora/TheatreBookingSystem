05289563f80a77a7b166b1ead956d268fac92c1d
Dragon-Aurora <susannah.elizabeth.dixon@gmail.com>
2022-04-03 16:03:40 +0100

    Added comments on how to connect to a SeatLabel clicked event

--- SeatLabel.py
+++ SeatLabel.py
@@ -6,12 +6,21 @@ from PyQt5.QtWidgets import *
 class SeatLabel(QLabel):
     """Subclass a QLabel so we can get the mouse press event
 
-    see https://stackoverflow.com/questions/60311219/easiest-way-to-subclass-a-widget-in-python-for-use-with-qt-designer
+    see
+    - https://stackoverflow.com/questions/60311219/easiest-way-to-subclass-a-widget-in-python-for-use-with-qt-designer
+    - https://stackoverflow.com/questions/962640/pyqt-qlabel-inheriting?rq=1
+
+
     """
+
+    # create a qt signal object
     clicked = pyqtSignal()
 
     def __init__(self, first_label):
         QLabel.__init__(self, first_label)
 
     def mousePressEvent(self, event):
+        #emit the signal
+        #to capture the signal use variable.clicked.connect(slotfunction)
+        #Slotfunction will get called when you click on a seat label
         self.clicked.emit()
