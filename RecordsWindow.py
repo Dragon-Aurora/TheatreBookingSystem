bba0ad42efdf8c1b0c6d134628cc3b2adb152e22
Dragon-Aurora <susannah.elizabeth.dixon@gmail.com>
2022-05-13 18:36:05 +0100

    All combo boxes have been filled with data. Beginning to add data to the table.

--- RecordsWindow.py
+++ RecordsWindow.py
@@ -36,9 +36,36 @@ class RecordsWindow(QMainWindow, Ui_Records):
         self.db_connection.open()
         # extract all data using cursor and put in UI
         Booking_cursor = self.db_connection.execute(BookingSQL).fetchall()
+        for item in Booking_cursor:
+
+        self.db_connection.close()
+
+        self.db_connection.open()
         Performance_cursor = self.db_connection.execute("SELECT * FROM tPerformance").fetchall()
+        for item in Performance_cursor:
+            # Get the Time of the performance - the index 1 is the second column which is Performance_Time
+            performanceTime = item[1].strftime('%H:%M')
+            # Get the date of the performance - third column Performance_Date
+            performanceDate = str(item[2])
+            # Format the value for adding to the combobox
+            value = performanceTime + " " + performanceDate
+            self.Performance_Combo.addItem(value)
+        self.db_connection.close()
+
+        self.db_connection.open()
         SeatID_cursor = self.db_connection.execute("SELECT * FROM tSeats").fetchall()
+        for item in SeatID_cursor:
+            SeatID = item[0]
+            self.SeatID_Combo.addItem(SeatID)
+        self.db_connection.close()
+
+        self.db_connection.open()
         Cust_cursor = self.db_connection.execute("SELECT * FROM tCustomer")
+        for item in Cust_cursor:
+            FirstName = item[1]
+            Surname = item[2]
+            fullname = FirstName+" "+Surname
+            self.Customer_Combo.addItem(fullname)
         self.db_connection.close()
 
         """rowCount = len(cursor)
