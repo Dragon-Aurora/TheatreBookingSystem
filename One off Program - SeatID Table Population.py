77d9490c5dca59577a175821c4c2a56627c741c1
Dragon-Aurora <susannah.elizabeth.dixon@gmail.com>
2022-05-14 09:51:19 +0100

    Increased the range for row from 0-9 to 0-10 and column from 0-19 to 0-20. Produced the amount of ID's wanted and are in order.

--- One off Program - SeatID Table Population.py
+++ One off Program - SeatID Table Population.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     db_connection = SQLServerAccess()
     # db_connection.open()
-    for row in range(0, 9):
-        for column in range(0, 19):
+    for row in range(0, 10):
+        for column in range(0, 20):
             ID = (row * max_columns) + column
             print(ID)
             # db_connection.execute("INSERT INTO tSeats values("+str(ID)+")")
