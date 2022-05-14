309a494d54920edb0cb0709eebc3e20a40efbad1
Dragon-Aurora <susannah.elizabeth.dixon@gmail.com>
2022-05-14 13:04:21 +0100

    SQL Tables were re-made so that the constraints were appropriate. The previous version of this program was not inputting the ID into the SQL table. Am now trying to do this through creating the entire SQL statement in a variable and then attempting to input the data into the SQL table.

--- One off Program - SeatID Table Population.py
+++ One off Program - SeatID Table Population.py
@@ -7,10 +7,11 @@ if __name__ == "__main__":
     import sys
 
     db_connection = SQLServerAccess()
-    db_connection.open()
+    #db_connection.open()
     for row in range(0, 10):
         for column in range(0, 20):
             ID = (row * max_columns) + column
-            print(ID)
-            db_connection.execute("INSERT INTO tSeats values("+str(ID)+")")
-    db_connection.close()
+            SQLStatement = "INSERT INTO tSeats VALUES("+str(ID)+")"
+            print(SQLStatement)
+            #db_connection.execute("INSERT INTO tSeats values("+str(ID)+")")
+    #db_connection.close()
